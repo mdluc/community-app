7f937dd372e3738f31fd049fd66de7bf6acb14c2
mdluc <madalinaluceac@gmail.com>
2022-03-01 12:42:59 +0200

    changed routing settings for git-pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import {store} from "./redux/store";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { store } from "./redux/store";
 import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
